2705538f2ae97e6064dd2f7d90df1467b5cb0c0c
StevenCh06 <68131651+StevenCh06@users.noreply.github.com>
2023-10-19 10:24:53 -0600

    AvanceTiendaSemana6

--- src/main/java/com/Tienda/service/CategoriaService.java
+++ src/main/java/com/Tienda/service/CategoriaService.java
@@ -7,8 +7,18 @@ import java.util.List;
 
 public interface CategoriaService {
     
+    
+    //Retorna una lista de caegorias (activas o todas)
     public List<Categoria> getCategorias(boolean activos); // Firma de un metodo
     
+    //Retorna na categoria por ID
     public Categoria getCategoria(Categoria categoria);
     
+    
+    // Se inserta un nuevo registros si el ID de la categoria esta vacío
+    //Se actualiza el registro si el ID de la categoria NO esta vacío
+    public void save (Categoria categoria);
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete (Categoria categoria);
 }
---
+++ src/main/java/com/Tienda/service/FirebaseStorageService.java
@@ -0,0 +1,23 @@
+
+package com.Tienda.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-bc688.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "Firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-bc688-firebase-adminsdk-b06vd-06b00b7b87.json";
+}
